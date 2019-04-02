002e284432f8d9de1d3cac4f90037bb94beca8b0
Mark Abramov <markabramov01@gmail.com>
2019-04-02 17:41:28 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Analog Clock
+A simple and accurate clock that can be placed on any webpage.
+
+Uses JavaScript, CSS, and an HTML document containing an SVG image of a clock.
+
+An instance of this clock is live [here](https://venus.cs.qc.cuny.edu/~abma2399/analog-clock/).
--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-const HOURHAND = document.querySelector("#hour");
-const MINUTEHAND = document.querySelector("#minute");
-const SECONDHAND = document.querySelector("#second");
+const HOUR_HAND = document.querySelector("#hour");
+const MINUTE_HAND = document.querySelector("#minute");
+const SECOND_HAND = document.querySelector("#second");
 
-var date = new Date();
-
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-
-let secPosition = sec * 360 / 60;
-let minPosition = min * 360 / 60 + secPosition / 60;
-let hrPosition = hr * 360 / 12 + minPosition / 12;
+let date = new Date(),
+    hr = date.getHours(),
+    min = date.getMinutes(),
+    sec = date.getSeconds(),
+    secPosition = sec * 360 / 60,
+    minPosition = min * 360 / 60 + secPosition / 60,
+    hrPosition = hr * 360 / 12 + minPosition / 12;
 
 function runClock() {
-
     hrPosition += 1/120;
     minPosition += 0.1;
     secPosition += 6;
-
-    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    HOUR_HAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTE_HAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECOND_HAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
-var interval = setInterval(runClock, 1000);
+let interval = setInterval(runClock, 1000);
